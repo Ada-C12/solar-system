2fe7ff6accdfc331bf2202b5fb5912333477aa73
geli-gel <angele.zam@gmail.com>
2019-08-19 15:30:01 -0700

    added summary method for planet class

--- planet.rb
+++ planet.rb
@@ -7,4 +7,9 @@ class Planet
     @distance_from_sun_km = distance_from_sun_km
     @fun_fact = fun_fact  
   end
+  def summary
+    return "#{@name.upcase} is #{@color}. It is "\
+      "#{@distance_from_sun_km.round} km away from the sun, and "\
+      "has a mass of #{@mass_kg.round} kg. #{@fun_fact}"
+  end
 end
