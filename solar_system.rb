b02f0a7cef5df4c3ebc8c4aeb3d97e089aaec76e
Janice Huang <janicehuang@gmail.com>
2019-08-19 16:05:41 -0700

    initialize Wave 2 code

---
+++ solar_system.rb
@@ -0,0 +1,16 @@
+class SolarSystem
+
+  attr_reader :star_name, :planets
+
+  def initialize (star_name)
+    @star_name = star_name
+    @planets = []
+  end
+
+  def add_planet(planet)
+    @planets << planet
+  end
+
+  def list_planets
+    return "Planets orbiting #{star_name}
+    #{"
