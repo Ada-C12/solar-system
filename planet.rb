e3b41fc15336a9ffa1effbde4ebe5e1b128e3a60
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-08-19 14:33:57 -0700

    Created class Planet and completed constructor

---
+++ planet.rb
@@ -0,0 +1,10 @@
+class Planet
+  
+  #constructor
+  def initiualize (name, color, mass_kg, distance_from_sum_km, fun_fact)
+    @name = name
+    @color = name
+    @mass_kg = mass_kg
+    @distance_from_sum_km = distance_from_sum_km
+    @fun_fact = fun_fact
+  end
