a48f07e6a78c92faa6a72a142bd96fa60b7bd0f6
KKennedyCodes <holmstedtk@gmail.com>
2019-08-19 16:53:56 -0700

    Return List_Planets for solar_system.rb

--- main.rb
+++ main.rb
@@ -1,11 +1,25 @@
 require_relative 'planet.rb'
+require_relative 'solar_system.rb'
 
 def main
+  solar_system = SolarSystem.new('Sol')
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
   mars = Planet.new('Mars', 'red', 6.39e23, 2.279e8, 'Named after the Roman God of War')
   
-  puts earth.summary
-  puts mars.summary
+  solar_system.add_planet(earth)
+  
+  list = solar_system.list_planets
+  puts list
 end
 
 main
+
+# solar_system = SolarSystem.new('Sol')
+
+# earth = Planet.new('Earth', ...)
+# solar_system.add_planet(earth)
+
+# list = solar_system.list_planets
+# puts list
+# # => Planets orbiting Sol
+# # => 1.  Earth
---
+++ solar_system.rb
@@ -0,0 +1,25 @@
+class SolarSystem
+  
+  attr_reader :star_name, :planets
+  
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+  
+  def add_planet(planet)
+    @planets.push(planet.name)
+    
+  end
+  
+  def list_planets
+    intro =  "Planets Orbiting #{@star_name}: \n"
+    count = 1
+    @planets.each do |x|
+      intro += "#{count}. #{x}\n"
+    end
+    
+    return intro
+  end
+  
+end
