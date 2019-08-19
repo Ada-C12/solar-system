79377c1de8cfb190badfd36822e8545b125d164b
Rinostar <codingrinostar@gmail.com>
2019-08-19 15:11:26 -0700

    Created two instances

--- main.rb
+++ main.rb
@@ -1,15 +1,13 @@
 require_relative "planet"
 
-# Load Planet into pry:
-# $ pry -r ./planet.rb
-earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life', "none")
 
-puts earth.name
-# => Earth
-puts earth.fun_fact
-# => Only planet known to support life
-puts earth.sailor_moon_guardian
 
-earth.color = 'pink'
-# => NoMethodError: undefined method `color=' for #<Planet:0x00007fcfba04c130>
-# => Did you mean?  color
+def main
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life', 'none (we are not cool enough)')
+  mercury = Planet.new('Mercury', 'dark-grey', 3.285e23, 5.791e7, 'Closest planet to sun', 'Sailor Mercury')
+
+  return "#{earth.name}'s Gaurdian: #{earth.sailor_moon_guardian}; #{mercury.name}'s Gaurdian: #{mercury.sailor_moon_guardian}"
+
+end
+
+puts main
