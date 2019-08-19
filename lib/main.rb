62bd09c5180f9db09a6aa8b98630fdd4301c7a0b
Nicky Choi <nickyjinchoi@gmail.com>
2019-08-19 15:10:34 -0700

    created new solarsystem, planet, and puts list

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,13 @@
 require_relative "planet"
 
 def main
+  solar_system = SolarSystem.new("Solar")
+
+  earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
+  solar_system.add_planet(earth)
+
+  list = solar_system.list_planets
+  puts list
 end
 
 main
