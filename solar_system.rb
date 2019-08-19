98056bafbcf7d5f255daf07a65fd695ed2f8d432
m.dove@live.com <m.dove@live.com>
2019-08-19 16:17:34 -0700

    Fixed bugs in solar_system

--- solar_system.rb
+++ solar_system.rb
@@ -2,32 +2,30 @@ require_relative "planet"
 
 class SolarSystem
   attr_reader :star_name, :planets, :planet
-  attr_reader :planets_list, :planets_list_formatted
+  attr_reader :planets_list, :planets_list_formatted, :planet_to_find
 
   def initialize(star_name)
     @star_name = star_name
-    @planets = planets
-    @planets = []
     @planets_list = []
-    @planet = planet
+    @planets = []
   end
 
   def add_planet(planet)
-    @planets_list.push(planet)
+    @planets.push(planet)
   end
 
   def list_planets
     i = 1
     "Planets orbiting #{star_name}"
-    @planets_list.each do |planet|
-      @planets_list = "#{i}. #{planet.name}"
+    @planets.each do |planet|
+      @planets_list.push("#{i}. #{planet.name}")
       return planets_list
       i += 1
     end
   end
 
-  def find_planet_by_name(planet)
-    @planets_list.each do |planet|
+  def find_planet_by_name(planet_to_find)
+    planets.each do |planet|
       if planet.name.downcase == planet_to_find.downcase
         return planet
       end
