7a761ae47a5ec5146513ed3d045b58c57e0f0000
Janice Huang <janicehuang@gmail.com>
2019-08-19 15:20:03 -0700

    get code to working state for Wave 1

---
+++ planet.rb
@@ -0,0 +1,24 @@
+class Planet
+
+  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+
+  def initialize (name, color, mass_kg, distance_from_sun_km, fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = check_number_validity(mass_kg, "mass")
+    @distance_from_sun_km = check_number_validity(distance_from_sun_km, "distance from sun")
+    @fun_fact = fun_fact
+  end
+
+  def summary
+    return "The glorious planet #{@name} is #{@color}-colored and has a mass of #{@mass_kg} kilograms. It is #{@distance_from_sun_km} kilometers from the sun. And wouldn't you like to know a fun fact about #{@name}? #{@fun_fact}"
+  end
+
+  def check_number_validity(num, measure)
+    unless num > 0
+      print "Positive number for #{measure}, please: "
+      num = gets.chomp
+    end
+    return num
+  end
+end
