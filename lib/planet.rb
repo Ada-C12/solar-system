2d0c5a144c96b71847fd6d2780d53e7b4b2e3f75
Nicky Choi <nickyjinchoi@gmail.com>
2019-08-19 14:36:27 -0700

    added constructor & parameters for wave 1

---
+++ lib/planet.rb
@@ -0,0 +1,19 @@
+class Planet
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+    @name = name
+    @color = color
+    if mass_kg < 0
+      raise ArgumentError, "ERROR. Mass must be greater than 0!"
+    end
+    @mass_kg = mass_kg
+    if distance_from_sun_km < 0
+      raise ArgumentError, "ERROR. Distance must be greater than 0!"
+    end
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
+  end
+
+  def summary
+    return "Planet #{name} is #{color}, weighs #{mass_kg}kg, is #{distance_from_sun_km}km from the sun. Fun fact: #{fun_fact}"
+  end
+end
