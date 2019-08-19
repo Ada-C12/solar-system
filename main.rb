371df71a43b4e66966d1b691664f75371609cb34
Ga-Young Jin <gyjinn@gmail.com>
2019-08-19 16:33:30 -0700

    Complete wave-2, added planet list and find planet

--- main.rb
+++ main.rb
@@ -2,11 +2,17 @@
 # Monday, August 19th, 2019
 # Week 3, Solar System Project
 
-require_relative "planet"
+require_relative 'planet'
+require_relative 'solar_system'
 
 def main
+  solar_system = SolarSystem.new('Sol')
+  
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
   mars = Planet.new('Mars', 'red', 6.39e23, 2.28e8, 'The planet is named after Mars, the Roman god of war')
+  
+  solar_system.add_planet(earth)
+  solar_system.add_planet(mars)
   
   puts earth.color
   puts earth.fun_fact
@@ -16,6 +22,11 @@ def main
   
   puts earth.summary
   puts mars.summary
+  list = solar_system.list_planets
+  puts list
+  
+  found_planet = solar_system.find_planet_by_name('eaRTH')
+  puts found_planet.summary
 end
 
 main
