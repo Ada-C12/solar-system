0e17d8c1f46e5a1617657bf0a7dbccc27ec4e441
Eve Le <nguyenthao.lee@gmail.com>
2019-08-19 16:02:27 -0700

    Added main.rb with 2 instances of Planet

---
+++ main.rb
@@ -0,0 +1,14 @@
+require_relative 'lib/planet'
+
+def main
+    names = %w(Earth Mars)
+    colors = %w(blue-green red-orange)
+    mass_kgs = [5.972e24, 6.417e23]
+    distances_from_sun_km = [1.496e8, 2.279e8]
+    fun_facts = ['Only planet known to support life', 'The planet is named after Mars, the Roman god of war']
+    2.times do |index|
+        planet = Planet.new(names[index], colors[index], mass_kgs[index], distances_from_sun_km[index], fun_facts[index])
+        puts "Planet ##{index + 1} is #{planet.name}, its color is #{planet.color}. Fun fact about it: #{planet.fun_fact}"
+    end
+end
+main
