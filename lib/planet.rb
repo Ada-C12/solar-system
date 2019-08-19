5b01ef48b7981a66d71b018c3294c97556f3b935
Eve Le <nguyenthao.lee@gmail.com>
2019-08-19 15:49:24 -0700

    Implemented constructor for Planet class

--- lib/planet.rb
+++ lib/planet.rb
@@ -1,2 +1,20 @@
 class Planet
+    attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+    
+    def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+        @name = name
+        @color = color
+        
+        if mass_kg <= 0
+            raise ArgumentError.new("Input must be a positive number")
+        end
+        @mass_kg = mass_kg
+        
+        if distance_from_sun_km <= 0
+            raise ArgumentError.new("Input must be a positive number")
+        end
+        @distance_from_sun_km = distance_from_sun_km
+        
+        @fun_fact = fun_fact
+    end
 end
