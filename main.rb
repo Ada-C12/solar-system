97fbf92f54c4c719d0cb8ca709ea881e89e1ab19
m.dove@live.com <m.dove@live.com>
2019-08-19 14:56:49 -0700

    Updated main to include call Add planet and list_planets and updated solar_system with adjusted varibale titles

--- main.rb
+++ main.rb
@@ -1,15 +1,13 @@
 require_relative "planet"
+require_relative "solar_system"
 
 def main
+  solar_system = SolarSystem.new("Sol")
   earth = Planet.new(name: "Earth", color: "blue-green", mass_kg: 5.972e24, distance_from_sun_km: 1.496e8, fun_fact: "Only planet known to support life")
 
-  puts earth.name
-  # => Earth
-  puts earth.fun_fact
-  # => Only planet known to support life
-
-  earth.color = "pink"
-  puts earth.summary
+  solar_system.add_planet(earth)
+  list = solar_system.list_planets
+  puts list
 end
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -1,20 +1,26 @@
 class SolarSystem
-  attr_reader :star_name, :planets
+  attr_reader :star_name, :planets, :planet
   attr_reader :planets_list, :planets_list_formatted
 
   def initialize(star_name)
     @star_name = star_name
     @planets = planets
     @planets = []
+    @planets_list = []
+    @planet = planet
   end
 
   def add_planet(planet)
-    @planets_list.push(planets)
-    i = 0
+    @planets_list.push(planet)
+  end
+
+  def list_planets
+    i = 1
     "Planets orbiting #{star_name}"
-    planets_list.each do |planet|
-      planets_list_formatted = "#{i}. #{planet.name}"
-      return planets_list_formatted
+    @planets_list.each do |planet|
+      @planets_list = "#{i}. #{planet.name}"
+      return planets_list
+      i += 1
     end
   end
 end
