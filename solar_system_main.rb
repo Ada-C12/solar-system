01cd99c7df4b8894ddaf49507ae6012949d5029a
Janice Huang <janicehuang@gmail.com>
2019-08-19 16:08:43 -0700

    revert to validity method call within constructor. tried to move to main but failed

--- solar_system_main.rb
+++ solar_system_main.rb
@@ -1,7 +1,7 @@
 require_relative 'planet'
 
 def main
-  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'It\'s the only planet known to support life.')
+  earth = Planet.new('Earth', 'blue-green', -5.972e24, 1.496e8, 'It\'s the only planet known to support life.')
 
   puts earth.summary
 end
