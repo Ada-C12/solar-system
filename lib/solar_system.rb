b62d7b322016d91b3ccbfc1d63a15e061a7a9850
Nicky Choi <nickyjinchoi@gmail.com>
2019-08-19 15:07:54 -0700

    created list_planets instance method

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -5,4 +5,16 @@ class SolarSystem
     @star_name = star_name
     @planets = []
   end
+
+  def add_planet(planet)
+    @planets << planet
+  end
+
+  def list_planets
+    list_of_planets = "Planets orbiting #{@star_name}"
+    @planets.each_with_index do |planet, num|
+      list_of_planets << "\n#{num + 1}. #{planet.name}"
+    end
+    return list_of_planets
+  end
 end
