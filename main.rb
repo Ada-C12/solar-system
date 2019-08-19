91cb9efa2dd584ac8d07e75a2194129891d9526e
eaball35 <eaball35@gmail.com>
2019-08-19 16:52:38 -0700

    Finished Wave 2

--- main.rb
+++ main.rb
@@ -6,12 +6,14 @@ require 'pry'
 def main
     solar_system = SolarSystem.new('Sol')
 
-    earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-    saturn = Planet.new('Saturn', 'yellow-brown', 5.683e26, 1.434e9, 'Best known for its fabulous ring system')
+    earth = Planet.new('earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+    saturn = Planet.new('saturn', 'yellow-brown', 5.683e26, 1.434e9, 'Best known for its fabulous ring system')
     
     solar_system.add_planet(earth)
     solar_system.add_planet(saturn)
-    puts solar_system.list_planets
+    list = solar_system.list_planets
+    found_planet = solar_system.find_planet_by_name("eArTh")
+    planetfound_planet.summary
 end
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -1,23 +1,27 @@
 require_relative "planet"
 
 class SolarSystem
+    attr_reader :star_name, :planets  
+
     def initialize(star_name)
         @star_name = star_name
         @planets = []
     end
-
-    attr_reader :star_name, :planets
-
+  
     def add_planet(planet)
         @planets << planet
     end
     
-    
     def list_planets
         planet_names = []
-        @planets.each_with_index do |planet,index| 
-            planet_names << "#{index + 1}. #{planet.name}"
-        end
+        @planets.each_with_index { |planet,index| planet_names << "#{index + 1}. #{planet.name}"}
         return "Planets orbiting #{star_name}\n#{planet_names.join("\n")}"
     end
-end   
+
+    def find_planet_by_name(planet)
+         @planets.each do |planet_instance|
+            return planet_instance if planet.upcase == planet_instance.name.upcase
+        end
+    end
+
+end  
