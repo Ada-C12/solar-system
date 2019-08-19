fa921f026e6e4cc4e6e66aeea24987992634a381
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-08-19 14:58:03 -0700

    created the summary method in in the class planet. Added earth input to main.rb so program runs. Works.

--- planet.rb
+++ planet.rb
@@ -3,10 +3,16 @@ class Planet
   attr_reader :name, :color, :mass_kg, :distance_from_sum_km, :fun_fact
   
   #constructor
-  def initiualize (name, color, mass_kg, distance_from_sum_km, fun_fact)
+  def initialize (name, color, mass_kg, distance_from_sum_km, fun_fact)
     @name = name
-    @color = name
+    @color = color
     @mass_kg = mass_kg
     @distance_from_sum_km = distance_from_sum_km
     @fun_fact = fun_fact
   end
+  
+  def summary
+    return "#{self.name} is a #{self.color} planet that weighs #{self.mass_kg} kg and is #{self.distance_from_sum_km} km from the sun. Fun fact: #{self.fun_fact}."
+  end
+  
+end
