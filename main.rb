5053a22a10eacd2c976f915869a4b5d8c2765e8f
geli-gel <angele.zam@gmail.com>
2019-08-19 15:20:33 -0700

    added main.rb file and main method

---
+++ main.rb
@@ -0,0 +1,15 @@
+require_relative "planet"
+
+def main
+  # create 1st instance of planet
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  # create 2nd instance of planet
+  mars = Planet.new('Mars', 'red', 6.39e23, 2.279e8, 'Once had water! Might still?')
+  # print out some attributes
+  planets = [earth,mars]
+  planets.each do |planet|
+    puts "#{planet.name} is #{planet.color}. It is #{planet.distance_from_sun_km.round} km away from the sun. #{planet.fun_fact}"
+  end
+end
+
+main
