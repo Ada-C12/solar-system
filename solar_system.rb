1e07c0b284e3a5fa93a29be330c0f20d8d20a2ed
Farah Davoodi <farahdavoodi@gmail.com>
2019-08-19 16:11:54 -0700

    Finished part 3 of Wave 2

--- solar_system.rb
+++ solar_system.rb
@@ -1,25 +1,29 @@
-require_relative 'planet'
+#require_relative 'planet'
 
 class SolarSystem
   
   attr_reader :star_name, :planets
-
+  
   def initialize(star_name)
     @star_name = star_name
     @planets = []
   end
-
+  
   def add_planet(planet)
     @planets << planet
   end
-
+  
   def list_planets
-    return "Planets orbiting the #{star_name} are #{@planets}."
-    #return "Planets orbiting #{@star_name}:"
-    
-    
-    # \n1.Mercury\n2.Venus\n3.Earth\n4.Mars\n5.Jupiter
+    planet_names = []
+    @planets.each_with_index do |planet,index|
+      planet_names << "#{index+1}. #{planet.name}"
+    end
+
+    return "Planets orbiting the #{star_name}:\n#{planet_names.join("\n")}"
+  
   end
 
-end
 
+end
+  
+  
