2ffc6fc9377017190bda40239aa228281598eb74
Nicky Choi <nickyjinchoi@gmail.com>
2019-08-19 16:07:21 -0700

    added find_planet_by_name & dist_between methods

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -17,4 +17,17 @@ class SolarSystem
     end
     return list_of_planets
   end
+
+  def find_planet_by_name(planet_name)
+    found_planets = @planets.find_all do |planet|
+      planet.name.casecmp? planet_name
+    end
+    return found_planets
+  end
+
+  def distance_between(planet_one, planet_two)
+    p_one = find_planet_by_name(planet_one)
+    p_two = find_planet_by_name(planet_two)
+    return p_one[0].distance_from_sun_km - p_two[0].distance_from_sun_km
+  end
 end
