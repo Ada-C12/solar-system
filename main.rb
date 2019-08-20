527af8d409f4910b721a500d1e020338ee3970c5
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-08-20 11:03:55 -0700

    main.rb after finishing wave 2

--- main.rb
+++ main.rb
@@ -17,9 +17,30 @@ def  main
     puts saturn.fun_fact 
     puts saturn.summary 
 
+    solar_system = SolarSystem.new('Sol')
+earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+saturn = Planet.new("Saturn", "Red", 5.683e26, 1.434e9, "Saturn is a flattened ball")
+neptune = Planet.new('Neptune', 'Blue', 1.024e26, 4.494e9, 'Neptune has rings')
+
+
+solar_system.add_planet(earth)
+solar_system.add_planet(saturn)
+solar_system.add_planet(neptune)
+
+
+
+list = solar_system.list_planets 
+puts list 
+
+found_planet = solar_system.find_planet_by_name("neptune")
+puts found_planet 
+
+
     
     
 end
+
+
 
 
 main 
