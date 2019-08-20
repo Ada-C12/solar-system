c8db6864bae642f5676ffc655635c9860adf72f5
Michaela Morrow <morrow.michaela@gmail.com>
2019-08-19 20:40:26 -0700

    Updated main.rb to call method 'list_planets' from solar_system.rb

--- lib/main.rb
+++ lib/main.rb
@@ -1,14 +1,43 @@
 require_relative "planet"
+require_relative "solar_system"
 
 def main
   
-  mars = Planet.new('Mars', 'red', 6.39e23, 2.279e8, 'Named after the Roman god of war')
+  solar_system = SolarSystem.new('Sol')
+  
+  mercury = Planet.new('Mercury', 'gray', 3.285e23, 5.791e7, 'A year on Mercury is 88 days long')
   venus = Planet.new('Venus', 'yellowish', 4.867e24, 1.082e8, 'Second brightest natural object in the sky')
-  # puts "\nPlanet 1: \nName: #{mars.name} \nColor: #{mars.color} \nMass (kg): #{mars.mass_kg} \nDistance from the sun (km): #{mars.distance_from_sun_km} \nFun Fact: #{mars.fun_fact}."
-  # puts "\nPlanet 2: \nName: #{venus.name} \nColor: #{venus.color} \nMass (kg): #{venus.mass_kg} \nDistance from the sun (km): #{venus.distance_from_sun_km} \nFun Fact: #{venus.fun_fact}."
-  puts mars.summary
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  mars = Planet.new('Mars', 'red', 6.39e23, 2.279e8, 'Named after the Roman god of war')
+  jupiter = Planet.new('Jupiter', 'orange', 1.898e27, 7.785e8, 'Has 67 moons')
+  saturn = Planet.new('Saturn', 'yellow-brown', 5.683e26, 1.434e9, 'Most distant planet that can be seen with the naked eye')
+  uranus = Planet.new('Uranus', 'blue', 8.681e25, 2.871e9, 'It takes 84 earth-years for Uranus to travel around the sun')
+  neptune = Planet.new('Neptune', 'bright blue', 1.024e26, 4.495e9, 'The coldest planet in the solar system')
+  
+  puts mercury.summary
   puts venus.summary
+  puts earth.summary
+  puts mars.summary
+  puts jupiter.summary
+  puts saturn.summary
+  puts uranus.summary
+  puts neptune.summary
+  
+  solar_system.add_planet(mercury)
+  solar_system.add_planet(venus)
+  solar_system.add_planet(earth)
+  solar_system.add_planet(mars)
+  solar_system.add_planet(jupiter)
+  solar_system.add_planet(saturn)
+  solar_system.add_planet(uranus)
+  solar_system.add_planet(neptune)
+  
+  list = solar_system.list_planets
+  puts list
   
 end
 
 main
+
+
+
