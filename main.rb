a6abf6928c583bd2d7dd7202fbcf8d61059c4ddc
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-08-20 10:58:27 -0700

    main.rb after wave one

---
+++ main.rb
@@ -0,0 +1,26 @@
+require_relative '../solar-system/lib/planet'
+require_relative '../solar-system/lib/solar_system'
+
+def  main 
+    neptune = Planet.new('Neptune', 'Blue', 1.024e26, 4.494e9, 'Neptune has rings')
+    
+    puts  neptune.name
+    puts  neptune.color 
+    puts  neptune.mass_kg 
+    puts  neptune.distance_from_sun_km  
+    puts neptune.fun_fact 
+    saturn = Planet.new("Saturn", "Red", 5.683e26, 1.434e9, "Saturn is a flattened ball")
+    puts  saturn.name
+    puts  saturn.color 
+    puts  saturn.mass_kg 
+    puts  saturn.distance_from_sun_km  
+    puts saturn.fun_fact 
+    puts saturn.summary 
+
+    
+    
+end
+
+
+main 
+
