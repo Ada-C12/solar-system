088595a464bc843aed0505db5ce91123e42c5a4c
Morgan Schuler <morganlschuler@gmail.com>
2019-08-20 16:54:21 -0700

    Finished Wave 2

--- main.rb
+++ main.rb
@@ -2,26 +2,24 @@ require_relative "solar_system"
 require_relative "planet"
 
 def main
-
-  # solar_system = SolarSystem.new('Sol')
-
-  # earth = Planet.new('Earth', "Mars", 'Jupiter', 'Pluto', 'Mercury')
-  # solar_system.add_planet(earth)
+  solar_system = SolarSystem.new('Sol')
   
-  # list = solar_system.list_planets
-  # puts list
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'is the only planet known to support life')
+  mars = Planet.new('Mars', 'red', 6.4219e23, 2.3e9, 'is named after the Roman god of war')
+  
+  solar_system.add_planet(earth)
+  solar_system.add_planet(mars)
+  
+  list = solar_system.list_planets
+  puts list
 
-earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'is the only planet known to support life')
-mars = Planet.new('Mars', 'red', 6.4219e23, 2.3e9, 'is named after the Roman god of war')
+found_planet = solar_system.find_planet_by_name('Earth')
+puts found_planet
 
-puts earth.summary
-puts mars.summary 
-
-#  list = list_planets
-
-return 
+  if found_planet != nil
+    puts found_planet.summary
+  end
 end
 
 main
-# 
-#6.4219e23, 2.3e9,
+
--- solar_system.rb
+++ solar_system.rb
@@ -1,13 +1,34 @@
 class SolarSystem
-attr_reader :star_name, :planets
-def initialize(star_name)
-  @star_name = star_name
-  @planets = []
-end
-def SolarSystem.add_planet(Planet)
-  Planet
-end
 
-def SolarSystem#list_planets
+  attr_reader :star_name, :planets
+
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+
+  def add_planet(planet)
+    @planets << planet
+  end
+
+  def list_planets
+    list_num = 1
+    list_string = "Planets orbiting #{star_name}:\n" 
+    @planets.each do |planet|
+      list_string << " #{list_num}.#{planet.name}\n"
+      list_num += 1
+    end
+    return list_string
+  end
+
+  def find_planet_by_name(check_planet)
+    @planets.each do |current_planet|
+      if current_planet.name.downcase == check_planet.downcase
+        return current_planet
+      else return nil
+      end
+    end 
+  end
 
 end
+    
