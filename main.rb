88d8a19fc4c11e6cdb423775ba2716421f74674e
Kristina <kristina.tanya@gmail.com>
2019-08-20 11:57:33 -0700

    Completed Wave 2 Requirements, starting Wave 3

--- main.rb
+++ main.rb
@@ -1,10 +1,7 @@
 require_relative 'planet'
-
-
-
+require_relative 'solar_system'
 
 def main
-  
   earth = Planet.new("Earth", "Blue-Green", 5.972e24, 1.496e8, "Only planet known to support life.")
   mars = Planet.new("Mars", "Red", 0.64171, 227.9, "Atmosphere is composed primarily of carbon dioxide.")
   
@@ -16,17 +13,23 @@ def main
   puts earth.summary
   puts mars.summary
   
+  # create an instance of SolarSystem and add all planets to the list
+  solar_system = SolarSystem.new('Sol')
   
+  solar_system.add_planet(mars)
+  solar_system.add_planet(earth)
   
+  list = solar_system.list_planets 
+  puts list
   
-  
-  
-  
-  
-  
+  found_planet = solar_system.find_planet_by_name('Earth')
+  puts found_planet
+  puts found_planet.summary
   
   
   
   
 end
+
 main
+
