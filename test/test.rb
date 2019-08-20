01fd8b9d152be49c6543c75d5a7b2fff6ab05369
gracexinran <gracexinran@gmail.com>
2019-08-20 11:19:12 -0700

    completed wave-2 test code

--- test/test.rb
+++ test/test.rb
@@ -7,6 +7,7 @@ require_relative '../lib/solar_system'
 
 Minitest::Reporters.use!
 
+# wave-1
 describe "Planet" do
   it "must provide a valid planet name" do 
     expect{Planet.new(2, 'blue-green', 11, 10, 'good')}.must_raise ArgumentError
@@ -40,24 +41,50 @@ describe "Planet" do
   
 end
 
-# describe "SolarSystem" do
-#   before do 
-#     @solar_system = SolarSystem.new('Sol')
-#     @earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+# wave-2
+describe "SolarSystem" do
+  before do 
+    @solar_system = SolarSystem.new('Sol')
+    @earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
     
-#     @solar_system.add_planet(@earth)
+    @solar_system.add_planet(@earth)
     
-#     @mars = Planet.new('Mars', 'brownish-red', 6.4171e23, 2.2794e8, 'The second smallest planet in the solar system behind Mercury')
+    @mars = Planet.new('Mars', 'brownish-red', 6.4171e23, 2.2794e8, 'The second smallest planet in the solar system behind Mercury')
     
-#     @solar_system.add_planet(@mars)
+    @solar_system.add_planet(@mars)
     
-    
-#   end
+  end
   
-#   describe "add planet" do
-#     it "must add valid planet to planets array" do
-#       expect{@solar_system.add_planet('planet')}.must_raise ArgumentError
-#     end
+  describe "add planet" do
+    it "must raise an error if argument is not an instance of Planet" do
+      expect{@solar_system.add_planet('planet')}.must_raise ArgumentError
+    end
     
-#   end
-# end
+    it "won't raise any error if the argument is valid" do
+      expect{@solar_system.add_planet(@earth)}.must_be_silent
+    end
+  end
+  
+  describe "list planets" do
+    it "returns a string listing all the planets in the planets array" do
+      expect(@solar_system.list_planets).must_equal "Planets orbiting Sol\n1. Earth\n2. Mars"
+    end
+  end
+  
+  describe "find planets by name" do
+    it "returns a message if the planet is not found" do
+      expect(@solar_system.find_planet_by_name('what?')).must_equal "This planet was not found!"
+    end
+
+    it "returns the object number if the planet is found and name variable is case-insentitive" do
+      expect(@solar_system.find_planet_by_name('eArth')).must_equal @earth
+    end
+  end
+  
+  describe "distance between two planets" do
+    it "return a string providing the absolute value of distance between two planets" do
+      expect(@solar_system.distance_between(@earth, @mars)).must_equal "The distance between Earth and Mars is 78340000.0km."
+    end
+  end
+
+end
