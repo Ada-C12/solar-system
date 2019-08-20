071e0e358f04facf3ac02f793952d222c3b13905
geli-gel <angele.zam@gmail.com>
2019-08-20 10:20:13 -0700

    updated main to set up small solar system and have cli for listing planets or exiting program

--- main.rb
+++ main.rb
@@ -2,23 +2,35 @@ require_relative "planet"
 require_relative "solar_system"
 
 def main
+  # Solar System Setup
+  system = SolarSystem.new("Sun")
+
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
   mars = Planet.new('Mars', 'red', 6.39e23, 2.279e8, 'Once had water! Might still?')
 
-  planets = [earth,mars]
+  planets = [earth, mars]
+
   planets.each do |planet|
-    puts "#{planet.summary}\n\n"
+    system.add_planet(planet)
   end
 
-  sun = SolarSystem.new("Sun")
-  sun.add_planet(earth)
-  sun.add_planet(mars)
-  puts sun.list_planets
+  # CLI
+  menu_options = ["list planets", "exit"]
+  user_input = ""
+  until user_input == "exit"
+    puts "\nWhat would you like to do next?"
+    puts menu_options.join("\n")
+    user_input = gets.chomp.downcase
+    case user_input
+    when "list planets"
+      puts "\n#{system.list_planets}"
+    when "exit"
+      puts "\nexiting solar system"
+    else
+      puts "\nplease enter an option from the menu."
+    end
+  end
 
-  found_planet = sun.find_planet_by_name("Earth")
-  puts "Searching for Earth.."
-  puts found_planet
-  puts found_planet.summary
 end
 
 main
