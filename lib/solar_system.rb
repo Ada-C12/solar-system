3320010c778bd505dd586abb45be7299a0d5e442
Michaela Morrow <morrow.michaela@gmail.com>
2019-08-19 17:21:09 -0700

    Defined method 'add_planet' in solar_system.rb

---
+++ lib/solar_system.rb
@@ -0,0 +1,14 @@
+class SolarSystem
+  
+  attr_reader :star_name, :planets
+  
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+  
+  def add_planet(Planet)
+    @planets << Planet
+  end
+  
+end
