9b6c601329393cf25b4e2026c88a8300eea05a6a
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-08-20 16:52:04 -0700

    added add planet method to main

--- main.rb
+++ main.rb
@@ -1,6 +1,30 @@
 require_relative 'planet'
 require_relative 'solar_system'
 
+
+def add_planet
+  puts "What is the planet's name?"
+  name = gets.chomp
+  
+  puts "What color is the planet?"
+  color = gets.chomp
+  
+  puts "What is its mass in kg?"
+  mass = gets.chomp.to_f
+  
+  puts "What is it's distance from the sun in km?"
+  distance = gets.chomp.to_f
+  
+  puts "And last, what's a fun fact about it?"
+  fun_fact = gets.chomp
+  
+  planet = Planet.new(name, color, mass, distance, fun_fact)
+  
+  return planet
+  
+end
+
+
 def main
   sun = SolarSystem.new("Sun")
   venus = Planet.new("Venus", "burnt sienna", 4.867e24, 108208930, "it is the second brightest object in the night sky")
@@ -11,18 +35,35 @@ def main
   sun.add_planet(venus)
   sun.add_planet(mars)
   
-  # options = ["list planets", "planet details", "add planet", "exit"]
-  # puts "Welcome to the solar system! What would you like to do?"
-  #   puts "You can either list planets or exit:"
   
-  #   selection = gets.chomp
-  #   until options.include?(selection.downcase) 
-  #     puts "I'm sorry, please choose either list planets or exit"
-  #     selection = gets.chomp
-  #   end
+  puts "Welcome to the solar system! What would you like to do?"
+    
+    run = true
+    
+    while run = true
+      
+      options = ["list planets", "planet details", "add planet", "exit"]
+      puts "You can choose: list planet, planet detials, add planet, or exit"
+      
+      selection = gets.chomp
+      until options.include?(selection.downcase) 
+        puts "I'm sorry, please choose an option or exit"
+        selection = gets.chomp
+      end
+      
+      case selection
+      when "list planets"
+        puts sun.list_planets
+      when "planet details"
+        puts sun.find_planet_by_name(selection)
+      when "add planet"
+        sun.add_planet
+      when "exit"
+        puts "See you next time!"
+        play = false
+      end
+      
+    end
+  end
+  main 
   
-  found_planet = sun.find_planet_by_name("mars")
-  
-  puts found_planet.summary
-end
-main 
