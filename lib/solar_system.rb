93e12dcf73e942b8f9916c287296563980eb5922
Eve Le <nguyenthao.lee@gmail.com>
2019-08-19 21:30:31 -0700

    Implemented list_planets for SolarSystem class

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -2,7 +2,7 @@ require_relative 'planet'
 
 class SolarSystem
     attr_reader :star_name, :planets
-    
+
     def initialize(star_name)
         @star_name = star_name 
         @planets = Array.new
@@ -12,5 +12,12 @@ class SolarSystem
         raise ArgumentError.new("Input must be a Planet!") if !(planet.instance_of? Planet)
         @planets << planet
     end
-    
+         
+    def list_planets
+        planets_list = "Planets orbiting #{@star_name}\n"
+        @planets.length.times do |index|
+            planets_list << "#{index + 1}. #{planets[index]}\n"
+        end
+        return planets_list
+    end
 end
