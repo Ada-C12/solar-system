5258146c719fd2fbb48b6c56484d47e436326c7a
Eve Le <nguyenthao.lee@gmail.com>
2019-08-19 22:08:12 -0700

    Added type error check for constructor and updated list_planets method for SolarSystem class

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -2,8 +2,9 @@ require_relative 'planet'
 
 class SolarSystem
     attr_reader :star_name, :planets
-
+    
     def initialize(star_name)
+        raise ArgumentError.new("Input must be a string for star name") if !(star_name.instance_of? String)
         @star_name = star_name 
         @planets = Array.new
     end
@@ -12,11 +13,11 @@ class SolarSystem
         raise ArgumentError.new("Input must be a Planet!") if !(planet.instance_of? Planet)
         @planets << planet
     end
-         
+    
     def list_planets
         planets_list = "Planets orbiting #{@star_name}\n"
         @planets.length.times do |index|
-            planets_list << "#{index + 1}. #{planets[index]}\n"
+            planets_list << "#{index + 1}. #{planets[index].name}\n"
         end
         return planets_list
     end
