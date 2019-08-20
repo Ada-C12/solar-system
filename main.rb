de8c5d4bf2e2bf29bfd66525ba89c033cc068cb2
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-08-20 16:27:24 -0700

    removed unnessecary code from find planet by name in solar system

--- main.rb
+++ main.rb
@@ -5,19 +5,24 @@ def main
   sun = SolarSystem.new("Sun")
   venus = Planet.new("Venus", "burnt sienna", 4.867e24, 108208930, "it is the second brightest object in the night sky")
   earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "I live on it")
-  mars = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "bees")
+  mars = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "it has a mountain 3x the size of Everest")
   
   sun.add_planet(earth)
   sun.add_planet(venus)
   sun.add_planet(mars)
   
-  puts sun.list_planets
+  # options = ["list planets", "planet details", "add planet", "exit"]
+  # puts "Welcome to the solar system! What would you like to do?"
+  #   puts "You can either list planets or exit:"
   
-  found_planet = sun.find_planet_by_name("earth")
+  #   selection = gets.chomp
+  #   until options.include?(selection.downcase) 
+  #     puts "I'm sorry, please choose either list planets or exit"
+  #     selection = gets.chomp
+  #   end
+  
+  found_planet = sun.find_planet_by_name("mars")
+  
   puts found_planet.summary
-  
 end
-
-
-main
-
+main 
--- solar_system.rb
+++ solar_system.rb
@@ -16,34 +16,12 @@ class SolarSystem
     @planets.each_with_index do |planet, index|
       list += "#{index + 1}. #{planet.name}""\n"
     end
-    p list
     return "Planets in #{@star_name}""\n" + list
     
   end 
   
   def find_planet_by_name(selection)
-    learn_more = @planets.select { |planet| planet.name.downcase == selection.downcase }
-    
-    if learn_more.count > 1
-      puts "There seems to be more than one planet with that name, here's both:"
-      learn_more.each_with_index do |planet, i|
-        puts "#{i + 1}. " + planet.summary
-      end
-      puts "Which one did you want?"
-      input = gets.chomp.to_i
-      while input < 1 || input > learn_more.count
-        puts "I'm sorry, please enter a number selection:"
-        input = gets.chomp.to_i
-      end
-      return learn_more[input - 1]
-      
-      
-    elsif learn_more.count == 1
-      return learn_more[0]
-    else
-      puts "I'm sorry, I don't know that planet"
-    end
-    
+    @planets.find { |planet| planet.name.downcase == selection.downcase}
   end
   
 end 
