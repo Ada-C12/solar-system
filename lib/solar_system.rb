7ef178cae2e9cf625886c7b3ff7c298fd2e65499
Michaela Morrow <morrow.michaela@gmail.com>
2019-08-19 20:39:07 -0700

    Created method 'list_planets' in solar_system.rb

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -7,8 +7,18 @@ class SolarSystem
     @planets = []
   end
   
-  def add_planet(Planet)
-    @planets << Planet
+  def add_planet(planet)
+    @planets << planet
+  end
+  
+  def list_planets
+    array_of_strings = ["\nPlanets orbiting #{@star_name}"]
+    
+    @planets.each_with_index do |planet, index|
+      array_of_strings << "\n#{index + 1}. #{planet.name}"
+    end
+    
+    return array_of_strings.join
   end
   
 end
