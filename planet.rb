9d971827bf06847aeef813837cc69a2f5a8af9a5
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-08-19 17:04:17 -0700

    Completed option portion that error checks that mass and distance are positive numbers.

--- planet.rb
+++ planet.rb
@@ -11,8 +11,25 @@ class Planet
     @fun_fact = fun_fact
   end
   
+  def mass_check (mass_kg)
+    while mass_kg < 0
+      print "Plant mass must be greater than zero. Mass: "
+      return @mass_kg = gets.chomp
+    end
+    return @mass_kg
+  end
+  
+  def distance_check (distance_from_sum_km)
+    while distance_from_sum_km < 0
+      print "Distance from sun must be greater than zero. Distance: "
+      return @distance_from_sum_km = gets.chomp
+    end
+    return @distance_from_sum_km
+  end
+  
+  
   def summary
-    return "#{self.name} is a #{self.color} planet that weighs #{self.mass_kg} kg and is #{self.distance_from_sum_km} km from the sun. Fun fact: #{self.fun_fact}."
+    return "#{self.name} is a #{self.color} planet that weighs #{mass_check(self.mass_kg)} kg and is #{distance_check(self.distance_from_sum_km)} km from the sun. Fun fact: #{self.fun_fact}."
   end
   
 end
