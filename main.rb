d0fc5bc3e1fb5289ab67fb0a124f6725d274f776
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-08-20 15:27:29 -0700

    added find planet by name method to solar system

--- main.rb
+++ main.rb
@@ -5,13 +5,16 @@ def main
   sun = SolarSystem.new("Sun")
   venus = Planet.new("Venus", "burnt sienna", 4.867e24, 108208930, "it is the second brightest object in the night sky")
   earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "I live on it")
+  mars = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "bees")
   
   sun.add_planet(earth)
   sun.add_planet(venus)
+  sun.add_planet(mars)
+  
   puts sun.list_planets
   
-  found_planet = sun.find_planet_by_name("EarTh")
-  p found_planet
+  found_planet = sun.find_planet_by_name("earth")
+  puts found_planet.summary
   
 end
 
