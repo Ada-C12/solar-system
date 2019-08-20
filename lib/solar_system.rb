ab4f473bf5085086121e857ee5896851d9799701
m.dove@live.com <m.dove@live.com>
2019-08-20 08:34:51 -0700

    Created lib directory for source code

---
+++ lib/solar_system.rb
@@ -0,0 +1,36 @@
+require_relative "planet"
+
+class SolarSystem
+  attr_reader :star_name, :planets, :planet
+  attr_reader :planets_list, :planets_list_formatted, :planet_to_find
+
+  def initialize(star_name)
+    @star_name = star_name
+    @planets_list = []
+    @planets = []
+  end
+
+  def add_planet(planet)
+    @planets.push(planet)
+  end
+
+  def list_planets
+    i = 1
+    planets_list = []
+    "Planets orbiting #{star_name}"
+    @planets.each do |planet|
+      planets_list.push("#{i}. #{planet.name}")
+      i += 1
+    end
+    return planets_list
+  end
+
+  def find_planet_by_name(planet_to_find)
+    @planets.each do |planet|
+      if planet.name.downcase == planet_to_find.downcase
+        return planet
+      end
+    end
+    return nil
+  end
+end
