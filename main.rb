5bb8ef0bb88a25533e61a1a962392f47e7abf88b
Rinostar <codingrinostar@gmail.com>
2019-08-20 16:39:35 -0700

    Finished wave#2

--- main.rb
+++ main.rb
@@ -1,13 +1,16 @@
 require_relative "planet"
-
-
+require_relative "solar_system"
 
 def main
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life', 'none (we are not cool enough)')
   mercury = Planet.new('Mercury', 'dark-grey', 3.285e23, 5.791e7, 'Closest planet to sun', 'Sailor Mercury')
 
-  puts earth.summary
-  puts mercury.summary
+  solar_system = SolarSystem.new('Sun')
+  solar_system.add_planet(earth)
+  
+  found_planet = solar_system.find_planet_by_name("eArth")
+  puts found_planet
+  puts found_planet.summary
 
 end
 
