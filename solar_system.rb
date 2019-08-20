cc7ecb9b98f236d5b2cd52cffbe86ba144ab06c4
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-08-20 16:30:52 -0700

    Created find_planet_by_name in my SolarSystem class.

--- solar_system.rb
+++ solar_system.rb
@@ -1,24 +1,35 @@
 class SolarSystem
   
-  attr_reader :star_name, :planets
+  attr_reader :star_name, :planets, :name
   
   #constructor
   def initialize (star_name)
     @star_name = star_name
     @planets = []
+    @name = name
   end
   
   def add_planet (cur_planet)
-    @planets << cur_planet.name
+    @planets << cur_planet
   end
   
   def list_planets
     count = 0
     puts "Planets orbiting #{@star_name}"
     list = @planets.map do |cur_planet|
-      planet = "#{count+=1}. #{cur_planet}"
+      planet = "#{count+=1}. #{cur_planet.name}"
     end
     return list
+  end
+  
+  def find_planet_by_name (name)
+    @planets.each do |planet|
+      if planet.name.downcase == name.downcase
+        return planet
+      else
+        raise ArgumentError, "Not a known planet"
+      end 
+    end 
   end
   
 end
