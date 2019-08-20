5c84f4ce0e6d73ff1a361186ba3320f163d280fa
Mariya Burrows <mariya.burrows@gmail.com>
2019-08-20 16:18:40 -0700

    working, complete draft 1 of solar system. commented out code = refactoring attempts that don't yet work.

---
+++ main_restructured.rb
@@ -0,0 +1,61 @@
+require_relative 'lib/planet'
+require_relative 'lib/solar_system'
+
+def main
+  sun = SolarSystem.new('Sun')
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  mars = Planet.new('Mars', 'red', 6.39e23, 227.9, "Named after the Roman god of war")
+  jupitar = Planet.new('Jupitar', 'white, red, orange, brown, yellow', 1.8982e1027, 778.5, 'cannot become a star')
+  saturn = Planet.new('Saturn', 'yellow-brown', 5.683e26, 1.4e9, "The rings surrounding Saturn are thin and not completely solid" )
+  sun.add_planet(earth)
+  sun.add_planet(mars)
+  sun.add_planet(jupitar)
+  sun.add_planet(saturn)
+  list = sun.list_planets
+ 
+  done = nil
+  until done
+    puts "Would you like to: list planets, add a planet or exit?"
+    choice = gets.chomp.downcase
+
+    if choice == "list planets"
+      puts list
+      puts "What planet do you want to learn about?"
+      planet_choice = gets.chomp.strip
+      planet_display = sun.find_planet_by_name(planet_choice)
+      puts planet_display.summary
+    elsif choice == "add a planet"
+      # puts "Great. You want to add a planet. We need some details."
+      # print "What is your planet's name? >"
+      # planet_name = gets.chomp.capitalize 
+      # puts add_new_planet(planet_name)
+      # sun.add_planet(user_planet) 
+      # puts "Thanks for your help! Here's our updated list:"
+      # puts sun.list_planets
+      puts "Great. You want to add a planet. We need some details."
+      print "What is your planet's name? >"
+      planet_name = gets.chomp 
+      print "What is the color(s) of #{planet_name}? >"
+      colors = gets.chomp 
+      print "What is the mass of #{planet_name} in km? >"
+      mass = gets.chomp.to_i
+      print "What is the distance from the sun, in km? >"
+      distance = gets.chomp.to_i
+      print "What is a fun fact about #{planet_name}? >"
+      fun_fact = gets.chomp
+      user_planet = Planet.new(planet_name, colors, mass, distance, fun_fact)
+      sun.add_planet(user_planet) 
+      puts "Thanks for your help! Here's our updated list:"
+      puts sun.list_planets
+    elsif choice == "exit"
+      puts "See you later!"
+      done = true
+    else
+      puts "Invalid Entry" 
+      done = true
+    end
+  end
+end
+
+main
+
