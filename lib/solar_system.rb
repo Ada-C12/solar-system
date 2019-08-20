a51a1764ce6da6f07afd61cbf2e296ac91ce2ab2
Eve Le <nguyenthao.lee@gmail.com>
2019-08-19 18:31:04 -0700

    Implemented add_planet method for SolarSystem class

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -1,7 +1,16 @@
+require_relative 'planet'
+
 class SolarSystem
     attr_reader :star_name, :planets
+    
     def initialize(star_name)
         @star_name = star_name 
         @planets = Array.new
     end
+    
+    def add_planet(planet)
+        raise ArgumentError.new("Input must be a Planet!") if !(planet.instance_of? Planet)
+        @planets << planet
+    end
+    
 end
