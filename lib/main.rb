de46fd59d37ea1f8c910d2670509dff6d0e83cf1
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-08-20 13:42:52 -0700

    Began wave 3 step 1

--- lib/main.rb
+++ lib/main.rb
@@ -3,13 +3,53 @@ require_relative 'solar_system'
 
 def main
   
-  earth = Planet.new("earth", "green", 55.9, 10, "earth is big")
-  solar_system = SolarSystem.new("Sol")
-  solar_system.add_planet(earth)
-  # return solar_system.list_planets
+  puts "BIG BANG!"
+  puts "Enter the name of a star:"
+  name = gets.chomp
+  solar_system = SolarSystem.new(name)
   
-  found_planet = solar_system.find_planet_by_name("krypton")
-  puts found_planet
-end
-
-main
+  puts "You've created a solar system around a star named #{solar_system.star_name}. Would you like to add a planet? (Y/N)"
+  y_or_n = gets.chomp.upcase
+  
+  while y_or_n == "Y"
+    puts "Enter the planet name:"
+    name = gets.chomp.to_s
+    puts "Enter the planet's color:"
+    color = gets.chomp.to_s
+    puts "Enter the planet's mass in kg"
+    mass = gets.chomp.to_f
+    puts "Enter the planet's distance from the sun in km:"
+    distance = gets.chomp.to_f
+    puts "Enter a fun fact about the planet:"
+    fun_fact = gets.chomp.downcase
+    planet = Planet.new(name, color, mass, distance, fun_fact)
+    solar_system.planets << planet
+    
+    puts "You've added the planet #{name}. Would you like to add another planet? (Y/N)"
+    y_or_n = gets.chomp.upcase
+  end
+  
+  puts "What would you like to do next? \n 1. list planets \n 2. exit \nPress 1 or 2:"
+    response = gets.chomp.to_i
+    until response == 1 || response == 2
+      puts "Please press 1 or 2:"
+      response = gets.chomp.to_i
+    end  
+    if response == 1
+      puts solar_system.list_planets
+    elsif response == 2
+      "Thanks for creating a solar system!"
+    end  
+    
+    # earth = Planet.new("earth", "green", 55.9, 10, "earth is big")
+    # solar_system = SolarSystem.new("Sol")
+    # solar_system.add_planet(earth)
+    # # return solar_system.list_planets
+    
+    # found_planet = solar_system.find_planet_by_name("krypton")
+    # puts found_planet
+    
+  end
+  
+  main
+  
