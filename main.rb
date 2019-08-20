f58d045e3e1b7e6411cde19cfc73b7bbd62c2770
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-08-20 13:05:13 -0700

    Created add_planet and list_plants in class SolarSystem. Updated main.rb to instantiate SolarSystem.

--- main.rb
+++ main.rb
@@ -1,9 +1,16 @@
 require_relative 'planet'
+require_relative 'solar_system'
 
 def main
+  solar_system = SolarSystem.new('Sol')
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life') 
   
-  puts earth.summary
+  solar_system.add_planet(earth)
+  
+  list = solar_system.list_planets
+  puts list
+  
+  
 end
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -1,9 +1,25 @@
 class SolarSystem
   
-  attr_reader :starname, :planets
+  attr_reader :star_name, :planets
+  
   #constructor
   def initialize (star_name)
     @star_name = star_name
     @planets = []
   end
+  
+  def add_planet (planet_name)
+    @planets << Planet.name
+  end
+  
+  def list_planets
+    count = 1
+    puts "Planets orbiting #{@star_name}"
+    @planets.each do |cur_planet|
+      return "#{count}. #{cur_planet}"
+      count += 1
+    end
+    
+  end
+  
 end
