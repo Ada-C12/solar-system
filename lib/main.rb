dbb136058b14722aad7ed5076b6c2e0e5c2408d6
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-08-20 22:26:11 -0700

    added list of planets to main, ask user if they want to use the list or input their own planets

--- lib/main.rb
+++ lib/main.rb
@@ -8,14 +8,37 @@ def main
   name = gets.chomp
   solar_system = SolarSystem.new(name)
   
-  puts "You've created a solar system around a star named #{solar_system.star_name}. Would you like to add a planet? (Y/N)"
+  puts "You've created a solar system around a star named #{solar_system.star_name}. Would you like to add some planets? (Y/N)"
   user_response = gets.chomp.upcase
+  
+  
+  def upload_generic_list_of_planets(system)
+    
+    earth = Planet.new("Earth", "blue-green", 597200000000000000000000, 92960000, "Earth is home to many diverse species of flora and fauna")    
+    
+    pluto = Planet.new("Pluto", "orange", 1309000000000000000000, 5906380000, "Pluto gets its name from the Roman god of the underworld")
+    
+    neptune = Planet.new("Neptune", "sea-green",  102413000000000000000000000, 2973000000, "Neptune's interior is primarily composed of ice and rock")
+    
+    mercury = Planet.new("Mercury", "silver", 33011000000000000000000, 57910000, "The earliest known observations of Mercury are from an Assyrian astronomer around the 14th century BC")
+    
+    krypton = Planet.new("Krypton", "green", 49250000000000000000000000000, 100000000, "Despite its name, Krypton is not made of the element Kryptonite")
+    
+    system.planets << earth
+    system.planets << neptune
+    system.planets << mercury
+    system.planets << pluto
+    system.planets << krypton
+    
+    run_control_loop(system, "Y")
+    
+  end
   
   
   def add_a_new_planet(system, response)
     
     while response == "Y"
-      puts "Enter the planet name:"
+      puts "Enter the planet's name:"
       name = gets.chomp.to_s
       puts "Enter the planet's color:"
       color = gets.chomp.to_s
@@ -39,20 +62,23 @@ def main
       puts "You've added the planet #{name}. Would you like to add another planet? (Y/N)"
       response = gets.chomp.upcase
     end
+    
     run_control_loop(system, response)
+    
   end
   
   
   def run_control_loop(system, response)
+    
     response = 0
     while response != 3
-      puts "What would you like to do next? \n 1. list planets \n 2. learn details about a planet \n 3. add a planet \n 4. exit \nPress 1, 2, 3, or 4:"
+      puts "What would you like to do next? \n 1. list planets \n 2. learn details about a planet \n 3. add a planet \n 4. exit \nSelect 1, 2, 3, or 4:"
       response = gets.chomp.to_i
       case response
       when 1
         puts system.list_planets
       when 2
-        puts "Which planet would you like to learn about?"
+        puts "Which planet would you like to learn about? Enter the planet's name:"
         chosen_planet = gets.chomp
         puts system.find_planet_by_name(chosen_planet)
       when 3
@@ -62,14 +88,21 @@ def main
         puts system.saturn
         exit
       else
-        puts "Please press 1, 2, 3, or 4:"
-        response = gets.chomp.to_i
+        puts "That's not an option."
       end  
     end    
+    
   end  
+
   
   if user_response == "Y"
-    add_a_new_planet(solar_system, user_response)
+    puts "Would you like to use a generic list of planets?(Y/N)"
+    user_second_response = gets.chomp.upcase
+    if user_second_response == "Y"
+      upload_generic_list_of_planets(solar_system)
+    else
+      add_a_new_planet(solar_system, user_response)
+    end
   elsif user_response == "N"
     run_control_loop(solar_system, user_response)
   else
