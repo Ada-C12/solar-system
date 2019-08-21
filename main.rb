444f2ba96886b70fec1421164262deb3ab5f1bef
dora1405 <li.dora05@gmail.com>
2019-08-20 21:51:49 -0700

    Wave 1-3 final draft, unless otherwise

--- main.rb
+++ main.rb
@@ -1,35 +1,6 @@
 require_relative 'planet'
 require_relative 'solar_system'
 
-# Wave 1 and Wave 2
-# def main
-#   venus = Planet.new('Venus', 'reddish-brown', 4.867e24, 1.082e8, 'This planet is named after the Roman goddess Venus')
-#   mars = Planet.new('Mars', 'red', 6.39e23, 2.279e8, 'The Mars rover, Opportunity, ended on February, 13, 2019')
-
-#   puts venus.summary
-#   puts mars.summary
-
-#   solar_system = SolarSystem.new('Sol')
-
-#   solar_system.add_planet(venus)
-#   solar_system.add_planet(mars)
-
-#   list = solar_system.list_planets
-#   puts list
-
-#   puts "************************************************"
-
-#   found_planet = solar_system.find_planet_by_name('VEnus')
-
-#   # found_planet is an instance of class Planet
-#   puts found_planet
-#   # => #<Planet:0x00007fe7c2868ee8>
-
-#   puts found_planet.summary
-# end
-
-# main
-
 # Wave 3
 
 # Restructure main method to do the following:
