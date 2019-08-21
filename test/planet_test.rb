2656c5afa88974be295c7ef0579ad9026d790627
Paige Davenport <davenport.paige@outlook.com>
2019-08-20 20:00:04 -0700

    Edited tests for Wave 1

--- test/planet_test.rb
+++ test/planet_test.rb
@@ -17,11 +17,5 @@ describe "planet" do
     expect {Planet.new("Omicron Persei 8", "green-blue", 5.972e24, 0, "Only planet where you can see stars at night")}.must_raise ArgumentError
 
   end
-
-  # it "checks_if_input_planet_exists_in_Planet_class" do
-    
-
-  #   expect {Planet.new("Pluto", "purple", 2.3e18, 5.7e56, "Used to be a planet")}.must_raise ArgumentError
-
-  # end
+  
 end
