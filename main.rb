10066768f6ace75945474a97f0a40ff793697c70
Eve Le <nguyenthao.lee@gmail.com>
2019-08-20 22:51:32 -0700

    Updated main.rb

--- main.rb
+++ main.rb
@@ -42,19 +42,19 @@ end
 def create_planet
   print "Please enter in your planet's name: "
   name = get_string_input_from_user
-
+  
   print "Please enter in your planet's color: "
   color = get_string_input_from_user
-
+  
   print "Please enter in your planet's mass_kg: "
   mass_kg = get_positive_number_from_user
-
+  
   print "Please enter in your planet's distance from the sun: "
   distance = get_positive_number_from_user
-
+  
   print "Please enter in your planet's fun fact: "
   fact = get_string_input_from_user
-
+  
   return Planet.new(name: name, color: color, mass_kg: mass_kg, distance: distance, fact: fact)
 end
 
@@ -65,14 +65,14 @@ def get_2_planet_names_from_user(solar_system)
   2.times do 
     print "Planet name: "
     planet = get_string_input_from_user
-
+    
     while solar_system.find_planet_by_name(planet) == nil
-      puts "This planet doesn't exist in our Solar System"
+      puts "#{planet} doesn't exist in our Solar System. This is our Solar System:"
       puts solar_system.list_planets 
       print "Planet name: "
       planet = get_string_input_from_user
     end
-
+    
     user_planet_names << planet
   end
   return user_planet_names
@@ -82,7 +82,7 @@ def main
   solar_system = SolarSystem.new('Sol')
   print "How many planets would you like to enter in: "
   number_of_planets = get_positive_number_from_user
-
+  
   number_of_planets.times do |i|
     planet = create_planet
     solar_system.add_planet(planet)
@@ -91,28 +91,36 @@ def main
   available_decisions = ["Planet details", "Add planet", "List planets", "Calculate distance", "Exit"]
   while true
     user_decision = get_decision(available_decisions)
-
+    
     case user_decision
     when "Calculate distance"
       planet_names = get_2_planet_names_from_user(solar_system)
       distance = solar_system.distance_between(planet_names[0],planet_names[1])
       puts "The distance between #{planet_names[0].capitalize} and #{planet_names[1].capitalize} is #{distance} km"
-    
+      
     when "Planet details"
       print "Please enter in name of a planet you want to look up in our Solar System: "
       user_planet = get_string_input_from_user 
       planet = solar_system.find_planet_by_name(user_planet)
-      result_message = (planet == nil) ? "Oops, this planet doesn't exist in our Solar System" : "This is the first result we found in our Solar System about #{user_planet.capitalize}:\n #{planet.summary}"
-      puts result_message
-    
+      
+      result_message = ""
+      if planet == nil 
+        result_message << "Oops, #{user_planet} doesn't exist in our Solar System\n" 
+        result_message << solar_system.list_planets 
+      else
+        result_message << "This is the first result we found in our Solar System about #{user_planet}:\n #{planet.summary}"
+      end
+      
+      puts result_message  
+      
     when "Add planet"
       planet = create_planet()
       solar_system.add_planet(planet)
       puts solar_system.list_planets
-    
+      
     when "List planets"
       puts solar_system.list_planets
-    
+      
     else
       exit
     end
