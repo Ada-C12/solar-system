52018f29ceca7f2b0e9f77c893e825e7c03fd9b6
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-08-20 18:54:21 -0700

    added keyword arguments

--- test/planet_test.rb
+++ test/planet_test.rb
@@ -10,24 +10,25 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 describe "Planet" do 
      # make a test for each attribute 
     it "check if first argument is used and string" do 
-        venus = Planet.new("Venus", "yellow", 500, 600,"blah")
+        venus = Planet.new(name: "Venus", color: "yellow", mass_kg: 500, distance_from_sun_km:5.6e25,fun_fact:"blah")
         expect (venus.name).must_be_instance_of String
         expect(venus.name).must_equal "Venus"
     end 
     it "check if mass_kg greater than 0 " do 
-        venus = Planet.new("Venus", "yellow", 5.5e23, 6.2e13,"blah")
+        venus = Planet.new(name:"Venus",color:"yellow",mass_kg: 5.5e23,distance_from_sun_km:5.6e25,fun_fact:"one day on venus is longer than a year")
+        
         expect(venus.mass_kg).must_be_instance_of Float 
         expect(venus.mass_kg).must_be :>, 0 
         
     end 
     
     it "check if distance_from_sun_km is greater than 0 " do 
-        venus = Planet.new("Venus", "yellow", 5.5e10, 6.6e12,"blah")
+        venus = Planet.new(name:"Venus",color:"yellow",mass_kg:5.5e23,distance_from_sun_km:5.6e25,fun_fact:"one day on venus is longer than a year")
         expect(venus.distance_from_sun_km).must_be_instance_of Float 
         expect(venus.distance_from_sun_km).must_be :>, 0 
     end 
-    it "" do 
-        venus = Planet.new("Venus", "yellow", 500, 600,"blah")
+    it "checks if string" do 
+        venus = Planet.new(name: "Venus", color: "yellow", mass_kg: 5.5e23, distance_from_sun_km: 5.6e25, fun_fact: "one day on venus is longer than a year")
         
         expect (venus.color).must_be_instance_of String
         expect(venus.color).must_equal "yellow"
@@ -37,13 +38,13 @@ describe "Planet" do
     it "raises argument error if mass_kg < 0 " do 
         
         
-        expect{Planet.new("Venus","yellow",0,600,"blah")}.must_raise ArgumentError 
+        expect{Planet.new(name: "Venus", color: "yellow",mass_kg: 0,distance_from_sun_km: 500 ,fun_fact:"one day on venus is longer than a year")}.must_raise ArgumentError 
         
         
     end 
 
     it "raises argument error if distance_from_sun < 0" do 
-        expect{Planet.new("Venus","yellow",5.5e23,0,"blah")}.must_raise ArgumentError
+        expect{Planet.new(name:"Venus",color:"yellow",mass_kg:5.5e23,distance_from_sun_km: 0,fun_fact:"one day on venus is longer than a year")}.must_raise ArgumentError
 
 
     end 
