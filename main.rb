89f7a0f0394dfb23c4994c88c465a01c09caa2a9
emaust <erikamaust@gmail.com>
2019-08-20 20:15:58 -0700

    Wave 3 functioning - needs testing

--- main.rb
+++ main.rb
@@ -14,29 +14,50 @@ def main
   solar_system.add_planet(mars)
   solar_system.add_planet(venus)
   
-  puts "Enter '1' to list planet names, '2' to see planet details, or '3' to exit."
+  puts "Enter '1' to list planet names, '2' to see planet details, '3' to enter a new planet, or '4' to exit."
   decision = gets.chomp.to_s
   
-  while decision != "3"
+  while decision != "4"
     
     if decision == "1"
       puts solar_system.list_planets
-      puts "Enter '1' to list planet names, '2' to see planet details, or '3' to exit."
+      puts "Enter '1' to list planet names, '2' to see planet details, '3' to enter a new planet, or '4' to exit."
       decision = gets.chomp.to_s
       
     elsif decision == "2"
       found_planet = solar_system.find_planet_by_name("earth")
       puts found_planet
       puts found_planet.summary
-      puts "Enter '1' to list planet names, '2' to see planet details, or '3' to exit."
+      puts "Enter '1' to list planet names, '2' to see planet details, '3' to enter a new planet, or '4' to exit."
       decision = gets.chomp.to_s
+      
+    elsif decision == "3"
+      puts "Let's put information about the planet you'd like to add."
+      
+      new_planet = []
+      planet_categories = ["name","color", "mass_in_kg", "distance_from_sun_km", "fun_fact"]
+      
+      planet_categories.each do |category|
+        puts "Enter the planet's #{category}."
+        category_info = gets.chomp.to_s
+        new_planet << category_info
+      end
+      
+      trash_garbage = Planet.new(*new_planet)
+      solar_system.add_planet(trash_garbage)
+      
+      
+      puts "Enter '1' to list planet names, '2' to see planet details, '3' to enter a new planet, or '4' to exit."
+      decision = gets.chomp.to_s
+      
+      
       
     elsif decision !="1" && decision !="2" && decision !="3"
       puts "Invalid entry."
       exit
     end
     
-    if decision == "3"
+    if decision == "4"
       exit
     end
     
