05791c7236b879bfd765e9d588efa6b3ff425fc0
emaust <erikamaust@gmail.com>
2019-08-20 17:46:00 -0700

    Correct planet storage and able to list

--- main.rb
+++ main.rb
@@ -4,13 +4,23 @@ require_relative 'solar_system'
 def main
   
   earth = Planet.new("Earth","mostly blue with white clouds", 5.97e24, 1.496e8, "It has bees")
-  mars = Planet.new("Mars", "rusty butterscotch", 10e23, 227883110.4, "Mars has the tallest mountain in the solar system (Olympus Mons)")
+  mars = Planet.new("Mars", "rusty butterscotch", 10e23, 227883110.4, "It does not have bees")
+  
+  puts earth.summary
+  puts mars.summary
+  
   
   solar_system = SolarSystem.new("Sun")
+  
+  
+  
   solar_system.add_planet(earth)
   solar_system.add_planet(mars)
   puts solar_system.list_planets
   
+  # puts solar_system.list_planets
+  # found_planet = solar_system.find_planet_by_name("earth")
+  # puts found_planet
 end
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -1,4 +1,5 @@
 require_relative 'planet'
+require 'pry'
 
 class SolarSystem
   
@@ -11,16 +12,30 @@ class SolarSystem
   end
   
   def add_planet(planet)
-    planets << planet.name
+    @planets << planet
   end
+  
+  
   
   def list_planets
-    return "Planets orbiting the star '#{star_name}':
-    1. #{@planets[0]}
-    2. #{@planets[1]}"
+    list_planet = []
+    @planets.each_with_index do |planet, index|
+      list_planet << "\n#{index+ 1}.  #{planet.name}" 
+    end
+    return "Planets orbiting #{@star_name} \n#{list_planet.join}"
   end
   
+  # def find_planet_by_name(planet)
+  #   puts "Enter a planet you would like to search:"
+  #   planet = gets.chomp.upcase
   
+  #   if 
+  #     @planets.include?(planet)
+  #     puts planet
+  #   else
+  #     puts "Sorry bud"
+  #   end 
+  # end
   
 end
 
