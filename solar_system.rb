14734be79baaf7cd73a9ddd9cbe561fff426dd42
Janice Huang <janicehuang@gmail.com>
2019-08-20 21:22:56 -0700

    Add wave 3 functions; need to work on big loop

--- solar_system.rb
+++ solar_system.rb
@@ -11,27 +11,55 @@ class SolarSystem
   end
 
   def list_planets
-    return "Planets orbiting #{star_name}
-    #{gen_list(@planets).join( )}" # Revisit this when multiple planets to add new lines in between if needed
+    return "Planets orbiting #{star_name}: \n#{gen_list(@planets).join( )}"
   end
 
   def gen_list(items_to_list)
     count = 1
     list = []
     items_to_list.each do |item|
-      list << "#{count}. #{item.name}"
-    count += 1
+      list << "#{count}. #{item.name} \n"
+      count += 1
     end
     return list
   end
 
-  def find_planet_by_name(questioned)
-    planets.map! {|cur_planet| cur_planet.name}
-    return planets
+  def find_planet_by_name(planet_sought)
+    # DO .UNIQ on name to get rid of duplicates?
+    @planets.each do |test_against|
+      if test_against.name.downcase == planet_sought.downcase
+        return test_against
+      end
+    end
+  end
+
+  def produce_planet_details
+    print "Which planet are you curious about? "
+    planet_sought = gets.chomp
+    found_planet = find_planet_by_name(planet_sought)
+    while found_planet.class == Array
+      puts "Sorry, I don't recognize that planet. Please try again."
+      print "What planet are you curious about? "
+      planet_sought = gets.chomp
+      found_planet = find_planet_by_name(planet_sought)
+    end
+    return found_planet.summary
+  end
+
+  def collect_planet_details
+    print "What is your planet's name? "
+    planet = gets.chomp
+    print "What color is #{planet}? "
+    color = gets.chomp
+    print "What is the mass (in kg) of #{planet}? "
+    mass_kg = gets.chomp.to_f
+    print "What is #{planet}'s distance from the sun? "
+    distance_from_sun_km = gets.chomp.to_f
+    print "What is a fun fact about #{planet}? "
+    fun_fact = gets.chomp
+    added_planet = Planet.new(planet, color, mass_kg, distance_from_sun_km, fun_fact)
+    puts "Summary of the latest planet added to our solar system: #{added_planet.summary}"
+    return added_planet
   end
 
 end
-
-
-
-
--- solar_system_main.rb
+++ solar_system_main.rb
@@ -5,12 +5,43 @@ def main
   solar_system = SolarSystem.new('Sun')
 
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'It\'s the only planet known to support life.')
-
   solar_system.add_planet(earth)
 
-  list = solar_system.list_planets
+  venus = Planet.new('Venus', 'cream', 4.867e24, 108.2e8, 'It has the longest rotation period of any planet in the solar system.')
+  solar_system.add_planet(venus)
 
-  puts solar_system.planets
+  mars = Planet.new('Mars', 'red', 6.42e23, 227.9e8, 'It has a thin atmosphere composed primarily of carbon dioxide.')
+  solar_system.add_planet(mars)
+
+  mercury = Planet.new('Mercury', 'grey', 3.3e23, 57.9e8, 'Its orbital period around the sun is the shortest of all the planets in the solar system.')
+  solar_system.add_planet(mercury)
+
+  print "\nHello! Welcome to the Solar System! Would you like to (A)-List Planets (B)-Read Planet Details (C)-Add a Planet or (D)-Exit: "
+  choice = gets.chomp.upcase
+
+  until ['A', 'B', 'C', 'D'].include?(choice)
+    print "Invalid input. Please enter 'A', 'B', 'C' or 'D': "
+    choice = gets.chomp.upcase
+  end
+# To loop, had originally called 'main' at the end of every 'when' block. I thought that was pretty clever, but the problem was that it was making a new solar system with each loop, so added planets disappeared.
+  case choice
+  when "A"
+    puts solar_system.list_planets
+    main
+  when "B"
+    puts solar_system.produce_planet_details
+    main
+  when "C"
+    new_planet = solar_system.collect_planet_details
+    puts new_planet
+    solar_system.add_planet(new_planet)
+    p solar_system
+    main
+  when "D"
+    puts "You are now leaving the Solar System!"
+    exit
+  else
+  end
 end
 
 main
